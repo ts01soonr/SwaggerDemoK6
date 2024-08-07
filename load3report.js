f03d796bf84f29f9e360208c8fb502e6eeb280b4
Fang Zhiyong <fzhiyong@datto.com>
2024-08-07 14:05:59 +0200

    update wsport and load script

--- load3report.js
+++ load3report.js
@@ -4,6 +4,8 @@ import { setInterval } from 'k6/experimental/timers';
 import {sleep} from 'k6';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
 import { Counter , Gauge, Rate, Trend } from "k6/metrics";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
 let MemCount = new Trend("memory");
 let CPUCount = new Trend("cpu2");
 const BASE_URL = __ENV.BASE_URL || 'ws://localhost:8080';
@@ -112,3 +114,12 @@ export function mem_cpu() {
 
     sleep(10);
 }
+
+export function handleSummary(data) {
+    const customTitle = 'CPU & Memory';
+    const reportTitle = `${customTitle} - ${new Date().toLocaleDateString()}`;
+  
+    return {
+        'CPU_Memory.html': htmlReport(data, { title: reportTitle }),
+    };
+  }
