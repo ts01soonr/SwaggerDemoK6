181e08ff373a0321b20408f099cacf3b027da9f6
Fang Zhiyong <fzhiyong@datto.com>
2024-08-07 12:55:09 +0200

    another try

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 if not exist soonr.jar play fget soonr.jar https://us.workplace.datto.com/filelink/6813-79cc5be9-b5c9d2ad2e-2
 if exist log\awp.log move log\awp.log log\awp_%RANDOM%.log
-start start_api.bat
+::start start_api.bat
 java -jar soonr.jar 2>&1 | mtee log\awp.log
