a51fdd94cfebf4607d03776b4247521c4816c446
Fang Zhiyong <fzhiyong@datto.com>
2024-08-11 15:34:26 +0200

    get rid of compile warning

--- SwaggerDemo/SwaggerDemo/Model/Remote.cs
+++ SwaggerDemo/SwaggerDemo/Model/Remote.cs
@@ -6,8 +6,8 @@
         public string Host { get; set; } = null!;
         public int Port { get; set; }
         public string Command { get; set; } = null!;
-        public string Result { get; set; }
+        public string Result { get; set; } = null!;
         
-        public string Usage { get;set; }
+        public string Usage { get;set; } = null!;
     }
 }
